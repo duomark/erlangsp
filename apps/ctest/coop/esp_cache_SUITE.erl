a1e659206a42eca83f751e81193fd2ed719f3e6e
Jay Nelson <jay@duomark.com>
2012-08-27 22:16:50 -0700

    Test all code paths through esp_cache:new_datum_node

--- apps/ctest/coop/esp_cache_SUITE.erl
+++ apps/ctest/coop/esp_cache_SUITE.erl
@@ -8,12 +8,12 @@
 -export([all/0, init_per_suite/1, end_per_suite/1]).
 
 %% Pipeline and fanout tests
--export([datum_node/1]).
+-export([datum_value/1]).
 
 %% Test procs for validating process message output
 %% -export([receive_pipe_results/0, receive_round_robin_results/2]).
  
-all() -> [datum_node].
+all() -> [datum_value].
 
 init_per_suite(Config) -> Config.
 end_per_suite(_Config) -> ok.
@@ -22,15 +22,35 @@ end_per_suite(_Config) -> ok.
 %%----------------------------------------------------------------------
 %% Coop Node tests
 %%----------------------------------------------------------------------
-datum_node(_Config) ->
+datum_value(_Config) ->
+
+    %% Get original value...
     Kill_Switch = coop_kill_link_rcv:make_kill_switch(),
-    Coop_Node = esp_cache:new_datum_node(Kill_Switch, 17),
+    {coop_node, _Node_Ctl_Pid, Node_Task_Pid} = Coop_Node = esp_cache:new_datum_node(Kill_Switch, 17),
     R1 = make_ref(),
     coop:relay_data(Coop_Node, {get_value, {R1, self()}}),
-    check_datum(R1).
+    17 = check_datum(R1),
+    R2 = make_ref(),
+    coop:relay_data(Coop_Node, {get_value, {R2, self()}}),
+    17 = check_datum(R2),
+    
+    %% Replace value and get new value...
+    R3 = make_ref(),
+    coop:relay_data(Coop_Node, {replace, 23, {R3, self()}}),
+    23 = check_datum(R3),
+    R4 = make_ref(),
+    coop:relay_data(Coop_Node, {get_value, {R4, self()}}),
+    23 = check_datum(R4),
+    
+    %% Check for expiration...
+    erlang:monitor(process, Node_Task_Pid),
+    coop:relay_data(Coop_Node, {expire, {foo, self()}}),
+    {exited, _Pid} = check_datum(foo).
 
 check_datum(Ref) ->
-    receive {Ref, 17} -> 17
+    receive
+        {Ref, Value} -> Value;
+        {'DOWN', _MRef, process, Pid, normal} -> {exited, Pid}
     after 1000 -> timeout
     end.
 
